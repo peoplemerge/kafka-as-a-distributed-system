9d7387b9b9b05fd81dd4922e9e2cfa95de18c155
Dave Thomas <dave.thomas@ticketmaster.com>
2023-03-21 12:45:22 -0700

    Add support for building on m1 macs

--- scripts/build_docker_images.sh
+++ scripts/build_docker_images.sh
@@ -9,7 +9,13 @@ echo
 echo "Build custom cp-zookeeper and cp-server images with 'tc' installed"
 for image in cp-zookeeper cp-server; do
   IMAGENAME=localbuild/${image}-tc:${CONFLUENT_DOCKER_TAG}
-  docker build --no-cache --build-arg CP_VERSION=${CONFLUENT_DOCKER_TAG} --build-arg REPOSITORY=${REPOSITORY} --build-arg IMAGE=$image -t $IMAGENAME -f ${DIR}/../Dockerfile ${DIR}/../.
+  if [ `uname -m` == 'arm64' ]; then
+    echo "Building for an m1 mac or similar cpu arch"
+    docker build --no-cache --build-arg CP_VERSION=${CONFLUENT_DOCKER_TAG} --build-arg REPOSITORY=${REPOSITORY} --build-arg IMAGE=$image -t $IMAGENAME -f ${DIR}/../Dockerfile-m1 ${DIR}/../.
+  else
+    echo "Building for an x86_64"
+    docker build --no-cache --build-arg CP_VERSION=${CONFLUENT_DOCKER_TAG} --build-arg REPOSITORY=${REPOSITORY} --build-arg IMAGE=$image -t $IMAGENAME -f ${DIR}/../Dockerfile ${DIR}/../.
+  fi
   docker image inspect $IMAGENAME >/dev/null 2>&1 || \
      { echo "Docker image $IMAGENAME not found. Please troubleshoot and rerun"; exit 1; }
 done
